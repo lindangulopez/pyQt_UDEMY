00d13e4b526a821d0495f86904cc7a39fe70f2d3
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-05-08 18:57:11 +0200

    to debug

--- Point.py
+++ Point.py
@@ -9,5 +9,5 @@ class Point:
             str += "EPSG code: {0}".format(self.srs)
             return str
 
-        def distance_to(self, point):
+        def distance_to(self, pt):
             return ((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2) ** 0.5
--- Point3D.py
+++ Point3D.py
@@ -1,4 +1,3 @@
-
 class Point3D(Point):
     def __int__(self, x, y, z, srs, units="Meters"):
         Point.__int__(self, x, y, srs)
@@ -11,14 +10,20 @@ class Point3D(Point):
             str += "EPSG code: {0}".format(self.srs)
             return str
 
-        def distance_to(self, point):
+        def distance_to(self, pt):
             return ((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2) ** 0.5
 
-        def distance3D_to(self, point):
+        def distance3D_to(self, pt):
             return ((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2 +(self.z -pt.z ** 2)) ** 0.5
 
         def get_units(self):
             return self.__units
 
         def set_units(self):
-            return self.__units
+            if units is ["Meters","Feet"]:
+                self.__units = units
+            else:
+                print("{0} is not a valid unit for elevation".format(units))
+
+
+
