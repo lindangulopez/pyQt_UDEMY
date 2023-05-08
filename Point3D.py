b207858884b7d851d1f135cb824622b0416b70bd
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-05-08 18:32:51 +0200

    getters and setters
    for units

--- Point3D.py
+++ Point3D.py
@@ -1,7 +1,9 @@
+
 class Point3D(Point):
-    def __int__(self, x, y, z, srs):
+    def __int__(self, x, y, z, srs, units="Meters"):
         Point.__int__(self, x, y, srs)
         self.z = z
+        self.__units = units
 
 
         def __str__(self):
@@ -10,4 +12,13 @@ class Point3D(Point):
             return str
 
         def distance_to(self, point):
+            return ((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2) ** 0.5
+
+        def distance3D_to(self, point):
             return ((self.x - pt.x) ** 2 + (self.y - pt.y) ** 2 +(self.z -pt.z ** 2)) ** 0.5
+
+        def get_units(self):
+            return self.__units
+
+        def set_units(self):
+            return self.__units
