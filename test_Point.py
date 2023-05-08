b5339219c441d39a0482a74d0b3cc2632385dba1
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2023-05-08 18:31:55 +0200

    Update test_Point.py
    getters & setters

--- test_Point.py
+++ test_Point.py
@@ -1,8 +1,9 @@
 import Point as pt
 
 pt1 = pt.Point(3, 12, 4326)
-pt2 = pt.Point(6, 16, 4326)
+pt2 = pt.Point(6, 16, 2200, 4326)
 
 print(pt1.srs)
 print(pt1.distance_to(pt2))
-print(pt2)
+print(type(pt2))
+print(pt2.get__units)
